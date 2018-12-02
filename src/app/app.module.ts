e3aaa5706581975d75f56aa74afa2c8d13c6b024
Emanuel Campos <emanuelcampos.contato@gmail.com>
2018-12-02 20:39:07 -0300

    criar componente explorar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './/app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ExplorarComponent } from './explorar/explorar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExplorarComponent
   ],
   imports: [
     BrowserModule,
