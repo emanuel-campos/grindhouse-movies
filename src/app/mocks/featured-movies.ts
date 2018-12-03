79124cb92c139dbc50c93d1383528880ee0527b0
Emanuel Campos <emanuelcampos.contato@gmail.com>
2018-12-03 16:22:41 -0300

    criar lista de filmes

---
+++ src/app/mocks/featured-movies.ts
@@ -0,0 +1,58 @@
+export const FEATUREDMOVIES = [
+  {
+    title: 'Vingadores: Guerra Infinita',
+    time: '135 min',
+    cover: './assets/images/movies/vingadores-guerra-infinita.png',
+    rating: 5,
+    favorite: 0
+  },
+  {
+    title: 'Brilho eterno de uma mente sem...',
+    time: '135 min',
+    cover: './assets/images/movies/brilho-eterno-de-uma-mente.png',
+    rating: 4,
+    favorite: 0
+  },
+  {
+    title: 'John Wick',
+    time: '120 min',
+    cover: './assets/images/movies/john-wick.png',
+    rating: 3,
+    favorite: 0
+  },
+  {
+    title: 'Baby Driver',
+    time: '120 min',
+    cover: './assets/images/movies/baby-driver.png',
+    rating: 4,
+    favorite: 0
+  },
+  {
+    title: 'Pantera Negra',
+    time: '90 min',
+    cover: './assets/images/movies/pantera-negra.png',
+    rating: 4,
+    favorite: 0
+  },
+  {
+    title: 'Sicario',
+    time: '130 min',
+    cover: './assets/images/movies/sicario.png',
+    rating: 2,
+    favorite: 0
+  },
+  {
+    title: 'Corra',
+    time: '100 min',
+    cover: './assets/images/movies/corra.png',
+    rating: 4,
+    favorite: 0
+  },
+  {
+    title: 'Mãe!',
+    time: '125 min',
+    cover: './assets/images/movies/mae.png',
+    rating: 4,
+    favorite: 0
+  }
+];
---
+++ src/app/mocks/most-viewed-movies.ts
@@ -0,0 +1,58 @@
+export const MOSTVIEWEDMOVIES = [
+  {
+    title: 'Logan',
+    time: '135 min',
+    cover: './assets/images/movies/logan.png',
+    rating: 5,
+    favorite: 1
+  },
+  {
+    title: 'Blade Runner 2049',
+    time: '135 min',
+    cover: './assets/images/movies/blade-runner-2049.png',
+    rating: 4,
+    favorite: 0
+  },
+  {
+    title: 'Moonlight',
+    time: '135 min',
+    cover: './assets/images/movies/moonlight.png',
+    rating: 3,
+    favorite: 0
+  },
+  {
+    title: 'Dunkirk',
+    time: '135 min',
+    cover: './assets/images/movies/dunkirk.png',
+    rating: 4,
+    favorite: 0
+  },
+  {
+    title: 'Capitão Fantástico',
+    time: '125 min',
+    cover: './assets/images/movies/capitao-fantastico.png',
+    rating: 4,
+    favorite: 0
+  },
+  {
+    title: 'Mulher Maravilha',
+    time: '90 min',
+    cover: './assets/images/movies/mulher-maravilha.png',
+    rating: 2,
+    favorite: 0
+  },
+  {
+    title: 'Cargo',
+    time: '100 min',
+    cover: './assets/images/movies/cargo.png',
+    rating: 2,
+    favorite: 0
+  },
+  {
+    title: 'A Chegada',
+    time: '80 min',
+    cover: './assets/images/movies/a-chegada.png',
+    rating: 3,
+    favorite: 0
+  }
+];
